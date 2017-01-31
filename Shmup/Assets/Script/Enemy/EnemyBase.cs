de2839828999db3c71bfb31df7ab8dad25ba61f1
Kevin <paledust@hotmail.com>
2017-01-30 21:18:44 -0500

    EnemyType
    Built a basic Type of enemy and will start to build more types of enemy on it

---
+++ Shmup/Assets/Script/Enemy/EnemyBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour {
+	protected float moveSpeed;
+	public AudioClip destroySound;
+	// Use this for initialization
+	protected void Start () {
+		if(!GetComponent<AudioSource>())
+			gameObject.AddComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		Move();
+	}
+
+	virtual protected void Move()
+	{
+		
+	}
+
+	virtual protected void Rotate()
+	{
+
+	}
+
+	virtual protected void ApplyDamage(float Damage)
+	{
+
+	}
+
+	protected void HitSound()
+	{
+		GetComponent<AudioSource>().PlayOneShot(destroySound);
+	}
+	protected void Kill()
+	{
+		Destroy(gameObject);
+	}
+
+
+}
---
+++ Shmup/Assets/Script/Enemy/Enemy_Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Follow : EnemyBase {
+	private Color32 ShipColor;
+	private GameObject player;
+	private int direction;
+	
+	void Start()
+	{
+		base.Start();
+		ShipColor = ColorChoose.ColorLibrary[(int)Random.Range(0,ColorChoose.ColorLibrary.Length)];
+		GetComponent<SpriteRenderer>().color = ShipColor;
+
+		GetComponent<TrailRenderer>().startColor = ShipColor;
+		GetComponent<TrailRenderer>().endColor = ShipColor;
+		player = GameObject.Find("SpaceShip");
+		direction = (int)Random.Range(0,2)*2-1;
+	}
+
+	override protected void Move()
+	{
+		transform.rotation = player.transform.rotation;
+		transform.position += player.GetComponent<Control>().getVelocity() * direction;
+	}
+}
